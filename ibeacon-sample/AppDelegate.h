da3e38ad5da6e1ea8f3faa81cf1930c853bb06a5
Eric Ito <eito@esri.com>
2014-03-23 14:56:41 -0700

    Initial Commit

---
+++ ibeacon-sample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ibeacon-sample
+//
+//  Created by Eric Ito on 3/23/14.
+//  Copyright (c) 2014 Esri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ibeacon-sample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ibeacon-sample
+//
+//  Created by Eric Ito on 3/23/14.
+//  Copyright (c) 2014 Esri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
